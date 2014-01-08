0a625ae3e6fbcbaa637e9aaf4c90c2bbef4bcf9b
Dodzi Dzakuma <nexocentric@yahoo.com>
2014-01-09 05:16:34 +0900

    made lots of progress on the character replacement method and started on the aggregate folder function

--- bash-scripting-tools.sh
+++ bash-scripting-tools.sh
@@ -17,13 +17,16 @@
 SCRIPT_NAME="Silver-Studded-Blue BASH Scripting Tools"
 SCRIPT_VERSION="0.01"
 SCRIPT_VERSION_NAME="azuki"
-SCRIPT_OPTION_FLAGS="DdhTtVv"
-SCRIPT_SELF_TEST=0
-SCRIPT_DEBUG=0
+SCRIPT_OPTION_FLAGS="D:d:hTtVv"
+SCRIPT_SELF_TEST_MODE=0
+SCRIPT_DEBUG_MODE=0
+SCRIPT_FUNCTION_TO_DEBUG=
+SCRIPT_VERBOSE_MODE=0
 
 #───────────────────────────────────────────────────────────────────────────────
 # Script Control Functions
 #───────────────────────────────────────────────────────────────────────────────
+
 #===========================================================
 # [author]
 # Dzakuma, Dodzidenu
@@ -51,6 +54,21 @@ displayScriptLicense()
 {
 	#MIT something somethimg something...
 	return 0
+}
+
+#===========================================================
+# [author]
+# Dodzi Y. Dzakuma
+# [summary]
+# [parameters]
+# [return]
+#===========================================================
+displayVerboseInformation()
+{
+	if [[ $# -lt 1 ]]; then
+		return
+	fi
+	printf "%s\n" "$*" >&2
 }
 
 #===========================================================
@@ -149,29 +167,28 @@ while getopts $SCRIPT_OPTION_FLAGS scriptOption; do
 	case "${scriptOption}" in
 		#debug
 		D | d )
-			SCRIPT_DEBUG=1
+			SCRIPT_DEBUG_MODE=1
+			SCRIPT_FUNCTION_TO_DEBUG="${OPTARG}"
 			break
-		;;
-		h )
-			displayScriptHeader
-			displayScriptUsage
-			cleanUp 1
-			exit
 		;;
 		# turn on self testing
 		T | t )
-			SCRIPT_SELF_TEST=1
+			SCRIPT_SELF_TEST_MODE=1
 			break
 		;;
 		# display the script usage menu on help or invalid argments
-		V | v )
+		h | V )
 			displayScriptHeader
-			if [[ "${scriptOption}" = "v" ]]; then
+			if [[ "${scriptOption}" = "V" ]]; then
 				exit
 			fi
 			displayScriptUsage
 			cleanUp 1
 			exit
+		;;
+		# verbose mode
+		v )
+						
 		;;
 	esac
 	#remove parsed options from the list
@@ -187,6 +204,45 @@ stringToArray()
 	printf "%s"	"$(echo $1 | sed 's/\(.\)/\1 /g')"
 }
 
+displayFunctionSummary()
+{
+	#getopts settings
+	local OPTIND
+	local FUNCTION_NAME="$1"
+	local FUNCTION_OPTION_FLAGS="$2"
+	shift 2 # so that it starts running like a normal function
+
+	#FUNCTION_OPTION_FLAGS=$(printf "%s" "${FUNCTION_OPTION_FLAGS}" | sed s/-//g)
+	
+	#function variables
+	local unformatedOptions=$(stringToArray "${FUNCTION_OPTION_FLAGS}")
+	local functionOption=
+
+	echo "${unformatedOptions[@]}"
+	FUNCTION_OPTION_FLAGS=
+	#parse the options like each one is required
+	for option in ${unformatedOptions[@]}; do
+		FUNCTION_OPTION_FLAGS="${FUNCTION_OPTION_FLAGS}${option}:"
+	done
+	echo "${FUNCTION_OPTION_FLAGS[@]}"
+	echo $#
+
+	#parse get opts
+	while getopts $FUNCTION_OPTION_FLAGS functionOption; do
+		case "${functionOption}" in
+			* )
+				printf "%s %s\n" "-${functionOption}" "${OPTARG}"
+			;;
+		esac
+		#remove the options that were parsed
+		shift $((OPTIND-1))
+	done
+
+	for requiredParameter in "$@"; do
+		printf "<%s>\n" "${requiredParameter}"
+	done
+}
+
 reverseString()
 {
 	if [[ $# -lt 1 ]]; then
@@ -199,9 +255,128 @@ reverseString()
 	printf "%s" "${reversedString}"
 }
 
+isInteger()
+{
+	if [[ $# -lt 1 ]]; then
+		return
+	fi
+
+	#if the number exists and is maches format
+	test "$1" && printf "%d" "$1" > /dev/null;
+}
+
+ignoredFile()
+{
+	#変数宣言
+	local fileName=$1
+ 
+	#ファイルは指定されていなかった
+	if [[ -z $fileName ]]; then
+		printf "ファイル名はしてされていない\n"
+		exit 1
+	fi
+ 
+	#ファイルリストを確認する
+	for ignored in "${INGNORE_LIST[@]}"; do
+		#無視リストに入っているかを確認
+		if [[ "$fileName" =~ $ignored ]]; then
+			#このファイルはtarしてはいけない			
+			return $INGNORE_FILE
+		fi
+	done
+ 
+	#このファイルはtarしてもよし
+	return $ADD_FILE
+}
+
+aggregateDirectory()
+{
+	#getopts settings
+	local OPTIND
+	local FUNCTION_OPTION_FLAGS="fiRrv"
+	
+	#function settings
+	local FUNCTION_USAGE="${FUNCNAME[0]} -${FUNCTION_OPTION_FLAGS} FOLDER PATH"
+	local GLOBIGNORE=.:..
+	local RECURSIVE_MODE=
+	local AGGREGATE_MODE="directories"
+
+	#function variables
+	local aggregatedContents=
+	local directory=
+	local directories=
+	local functionOptionFlags=
+	local files=
+	local path=
+
+	#parse get opts
+	while getopts $FUNCTION_OPTION_FLAGS functionOption; do
+		case "${functionOption}" in
+			f )
+				functionOptionFlags="${functionOptionFlags}${OPTARG}"
+				AGGREGATE_MODE="files"
+			;;
+			# display the script usage menu on help or invalid argments
+			h )
+				displayVerboseInformation "Function usage:" 
+				displayVerboseInformation "${FUNCTION_USAGE}"
+				return 0
+			;;
+			#parse the directory recursively
+			R | r )
+				displayVerboseInformation "Replacement by individually matching characters has been set."
+				RECURSIVE_MODE=1
+				functionOptionFlags="${functionOptionFlags}${OPTARG}"
+			;;
+			# #display verbose information
+			v )
+				displayVerboseInformation "Verbose mode has been enabled."
+			;;
+			\? )
+				printf "-%s is an invalid option. \nPlease use the -h flag to display usage information.\n" "$OPTARG"
+				return 1
+			;;
+		esac
+		#remove the options that were parsed
+		shift $((OPTIND-1))
+	done
+
+	if [[ $# -lt 1 ]]; then
+		return
+	fi
+ 
+	for path in "$directory/"*; do
+ 
+		#無視すべきファイルを確認する
+		#ignoredFile "$path"
+		# if [[ $? -eq $INGNORE_FILE ]]; then
+		# 	printf "\n$pathを無視した\n無視リストにはいていた\nご確認ください\n\n"
+		# 	continue
+		# fi
+ 
+		#ディレクトリとファイルを別々の配列に保存
+		if [[ -f "$path" ]]; then
+			#ファイル
+			files=("${files[@]}" "$path")
+		elif [[ -d "$path" ]]; then
+			#ディレクトリ
+			directories=("${directories[@]}" "$path")
+			if [[ -n "${RECURSIVE_MODE}" ]]; then
+				if [[ -n "${functionOptionFlags}" ]]; then
+					functionOptionFlags="-${functionOptionFlags}"
+				fi
+				$(aggregateDirectory "${functionOptionFlags}" "${path}")
+			fi
+		fi
+	done
+
+	aggregatedContents=${!AGGREGATE_MODE}
+	printf "%s" "${aggregatedContents[@]}"
+}
+
 #===========================================================
 # [author]
-# Dzakuma, Dodzidenu
+# Dodzidenu Dzakuma
 # [summary]
 # [parameters]
 # [return]
@@ -210,73 +385,72 @@ replaceString()
 {
 	#getopts settings
 	local OPTIND
-	local FUNCTION_OPTION_FLAGS="chtv1"
-	local FUNCTION_USAGE="${FUNCNAME}"
+	local FUNCTION_OPTION_FLAGS="cfhn:tv"
+	local FUNCTION_USAGE="${FUNCNAME[0]} -${FUNCTION_OPTION_FLAGS} STRING SEARCH-STRING [REPLACEMENT STRING]"
 
-	#evaluation for the next three defines will be based on
-	#whether the string is empty or not
+	#function constants
 	local HEAD_REPLACE=1
 	local WHOLE_WORD_REPLACE=1 #default is on
 	local REPLACE_ALL_MATCHING="g"
-	local REPLACEMENT_DIRECTION="#%" #front/tail
-	local SPACE_PLACEHOLDER="█"
+	local SPACE_PLACEHOLDER="█" #this character will no normally appear in strings
+	local REPLACE_NTH_INSTANCE=0
+
+	#function variables
 	local functionOption=
 	local originalString=
 	local searchString=
 	local searchCharacters=
 	local replacementString= #will delete characters/words by default
 	local processedString=
-	local newString=
-	local characterList=
 	local character=
 	local processingString=
+	local nextReplacementCharacter=0
+	local extractionStart=
+	local extractionFinish=
 
-	#probably need getopts as you need to decide
-	#if you want whole string replacement or something else
-	#replacement of letters in string
-	#replacement of whole words in string
-	#-c for characters
-	#else whole word
-	#if replacement characters is a list
-	#will replace the characters one by one changing
-	#on different iterations
-	#-1 for first repetition whole word is one word
-	#otherwise character is one loop through????? possible??? maybe not possible for
-	#characters
-	#-t from tail
+	#parse get opts
 	while getopts $FUNCTION_OPTION_FLAGS functionOption; do
 		case "${functionOption}" in
 			#replace by character and not by whole word
 			c )
+				displayVerboseInformation "Replacement by individually matching characters has been set."
 				WHOLE_WORD_REPLACE=
+			;;
+			#replace first instance only
+			f )
+				displayVerboseInformation "Only the first match will be replaced."
+				REPLACE_ALL_MATCHING= #turn off all replacement
 			;;
 			# display the script usage menu on help or invalid argments
 			h )
-				printf "%s\n" "${FUNCTION_USAGE}"
-				return
+				displayVerboseInformation "Function usage:" 
+				displayVerboseInformation "${FUNCTION_USAGE}"
+				return 0
+			;;
+			# replace the nth instance of match
+			n )
+				isInteger "${OPTARG}"
+				if [[ $? -ne 0 ]]; then
+					displayVerboseInformation "Invalid argument for -n ${OPTARG}"
+					displayVerboseInformation "Function usage:" 
+					displayVerboseInformation "${FUNCTION_USAGE}"
+				fi
+				#OPTARG has been determined a safe integer so use it as such
+				REPLACE_NTH_INSTANCE=$OPTARG
 			;;
 			#start replacement from tail
 			t )
+				displayVerboseInformation "Replacement will begin from tail."
 				HEAD_REPLACE=
 			;;
 			# #display verbose information
-			# v )
-			# 	printf "%s\n" "BASS, BASS, BASS"
-			# ;;
-			# #replace first instance only
-			1 )
-				REPLACE_ALL_MATCHING= #turn off all replacement
+			v )
+				displayVerboseInformation "Verbose mode has been enabled."
 			;;
-			# * )
-			# 	#get non option args
-			# 	if [[ -z originalString ]]; then
-			# 		originalString="$OPTARG"
-			# 	elif [[ -z searchString ]]; then
-			# 		searchString="$OPTARG"
-			# 	else
-			# 		replacementString="$OPTARG"
-			# 	fi
-			# ;;
+			\? )
+				printf "-%s is an invalid option. \nPlease use the -h flag to display usage information.\n" "$OPTARG"
+				return 1
+			;;
 		esac
 		#remove the options that were parsed
 		shift $((OPTIND-1))
@@ -294,6 +468,16 @@ replaceString()
 	originalString=$1
 	originalString=${originalString// /$SPACE_PLACEHOLDER}
 
+	displayVerboseInformation \
+		"Searching [${originalString//$SPACE_PLACEHOLDER/ }] " \
+		"for [${searchString//$SPACE_PLACEHOLDER/ }] and replacing " \
+		"from [${replacementString//$SPACE_PLACEHOLDER/ }]." 
+	displayVerboseInformation "[Settings]"
+	displayVerboseInformation "Character replace  : " \
+	displayVerboseInformation "Replace from tail  : " \
+	displayVerboseInformation "First match replace: "
+	displayVerboseInformation ""
+
 	#safety checks
 	if [[ -z "${originalString}" ]] || [[ -z "${searchString}" ]]; then
 		return
@@ -308,19 +492,22 @@ replaceString()
 		fi
 	fi
 
+	#
+
 	#replacement loop
-	# processingString="/${searchString}/${replacementString}"
-	# while [[ -n "${processingString}" ]]; do
-	# 	originalString=${originalString$processingString}
 	if [[ -z "${WHOLE_WORD_REPLACE}" ]]; then
 		processingString="${originalString}"
 		searchCharacters=$(stringToArray "${searchString}")
-		#printf "{%s}" "$searchCharacters"
 		for character in $searchCharacters; do
 			processingString=$( \
 				printf "%s" "${processingString}" | \
-				sed "s/${character}/${replacementString}/${REPLACE_ALL_MATCHING}" \
+				sed "s/${character}/${replacementString:$nextReplacementCharacter:1}/${REPLACE_ALL_MATCHING}" \
 			)
+			if [[ $nextReplacementCharacter -eq $((${#replacementString} - 1)) ]]; then #change this to -gt ${#replacementString} for meeting on the 15th
+				nextReplacementCharacter=0
+			else
+				nextReplacementCharacter=$((nextReplacementCharacter + 1))
+			fi
 		done
 		processedString="${processingString}"
 	else
@@ -328,6 +515,10 @@ replaceString()
 			printf "%s" "${originalString}" | \
 			sed "s/${searchString}/${replacementString}/${REPLACE_ALL_MATCHING}" \
 		)
+	fi
+
+	if [[ "${originalString}" = "processedString" ]]; then
+		displayVerboseInformation "No matches found in string."
 	fi
 
 	#reverse string for reverse lookup
@@ -355,7 +546,7 @@ replaceString()
 # 		case "${functionOption}" in
 # 			# turn on self testing
 # 			a )
-# 				SCRIPT_SELF_TEST=1
+# 				SCRIPT_SELF_TEST_MODE=1
 # 				break
 # 			;;
 # 			# display the script usage menu on help or invalid argments
@@ -410,7 +601,7 @@ displayDynamicTable()
 #───────────────────────────────────────────────────────────────────────────────
 # Function Test
 #───────────────────────────────────────────────────────────────────────────────
-if [[ $SCRIPT_SELF_TEST -eq 1 ]]; then
+if [[ $SCRIPT_SELF_TEST_MODE -eq 1 ]]; then
 
 testRepeatString()
 {
@@ -432,28 +623,36 @@ testReplaceString()
 	local originalString="clean up this lean mess!"
 	local replacedString=""
 
+	#help displayed
+	replacedString=$(replaceString -hv)
+	assertSame "Failed to display help." 0 $?
+
 	#insufficient parameters
-	replacedString=$(replaceString "${originalString}")
+	replacedString=$(replaceString -v "${originalString}")
 	assertNull "Failed to return null string." "${replacedString}"
 
 	#character replacement single
-	replacedString=$(replaceString -c1 "${originalString}" "c")
+	replacedString=$(replaceString -vcf "${originalString}" "c")
 	assertSame "Failed to replace first matching character in string." "lean up this lean mess!" "${replacedString}"
 
 	#multiple character replacement single
-	replacedString=$(replaceString -c1 "${originalString}" "cup ")
+	replacedString=$(replaceString -vcf "${originalString}" "cup ")
 	assertSame "Failed to replace first matching character in string." "lean this lean mess!" "${replacedString}"
 
 	#multiple character replacement all
-	replacedString=$(replaceString -c "${originalString}" "ethsm")
+	replacedString=$(replaceString -vc "${originalString}" "ethsm")
 	assertSame "Failed to replace all matching characters in string." "clan up i lan !" "${replacedString}"
 
-	#multiple character replacement all from tail
-	replacedString=$(replaceString -c1t "${originalString}" "ss")
-	assertSame "Failed to replace first matching character in string." "clean up this lean me!" "${replacedString}"
+	#multiple character replacement all
+	replacedString=$(replaceString -vcf "${originalString}" "isss" "at")
+	assertSame "Failed to replace single matching characters in string." "clean up that lean meat!" "${replacedString}"
+
+	#multiple character single from tail
+	replacedString=$(replaceString -vcft "${originalString}" "ss" "at")
+	assertSame "Failed to replace single matching characters in string from tail." "clean up this lean meat!" "${replacedString}"
 
 	#full word replacement single
-	replacedString=$(replaceString -1 "${originalString}" "clean" "beam")
+	replacedString=$(replaceString -vf "${originalString}" "clean" "beam")
 	assertSame "Failed to replace first matching whole word in string." "beam up this lean mess!" "${replacedString}"
 
 	#full word replacement all
@@ -461,7 +660,7 @@ testReplaceString()
 	assertSame "Failed to replace all matching whole words in string." "cup this mess!" "${replacedString}"
 
 	#full word replacement single from tail
-	replacedString=$(replaceString -1t "${originalString}" "lean ")
+	replacedString=$(replaceString -vft "${originalString}" "lean ")
 	assertSame "Failed to replace all matching whole words in string." "clean up this mess!" "${replacedString}"
 }
 
@@ -470,17 +669,20 @@ testReplaceString()
 
 fi
 
-if [[ $SCRIPT_DEBUG -eq 1 ]]; then
-	printf "Start debug scripts.\n"
-
-	debugOutput=$(replaceString -c "something" "some" "nothing")
-
+if [[ $SCRIPT_DEBUG_MODE -eq 1 ]]; then
+	printf "Start debugging scripts.\n"
+	#this right now is unsafe!!
+	eval $SCRIPT_FUNCTION_TO_DEBUG
 	exit 0
 fi
 #───────────────────────────────────────────────────────────────────────────────
 # Function Exports
 #───────────────────────────────────────────────────────────────────────────────
+export -f isInteger
+export -f displayFunctionSummary
 export -f repeatString
+export -f reverseString
+export -f replaceString
 
 #═══════════════════════════════════════════════════════════════════════════════
 # The MIT License (MIT)
