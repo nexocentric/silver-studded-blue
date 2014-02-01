a76c02e8ab7cf5091b21851ed3eb4cb83604f55f
Dodzi Dzakuma <nexocentric@yahoo.com>
2014-02-01 22:47:19 +0900

    making lots of progress on my colors

--- bash-colorization-tools.sh
+++ bash-colorization-tools.sh
@@ -1,60 +1,329 @@
 #!/bin/bash
 
+#script variables
+a0=(0 "name")
+a1=(1 "name")
+a2=(2 "name")
+a3=(3 "name")
+a4=(4 "name")
+a5=(5 "name")
+a6=(6 "name")
+a7=(7 "name")
+a8=(8 "name")
+a9=(9 "name")
+a10=(10 "name")
+a11=(11 "name")
+a12=(12 "name")
+a13=(13 "name")
+a14=(14 "name")
+a15=(15 "name")
+a16=(16 "name")
+a17=(17 "name")
+a18=(18 "name")
+a19=(19 "name")
+a20=(20 "name")
+a21=(21 "name")
+a22=(22 "name")
+a23=(23 "name")
+a24=(24 "name")
+a25=(25 "name")
+a26=(26 "name")
+a27=(27 "name")
+a28=(28 "name")
+a29=(29 "name")
+a30=(30 "name")
+a31=(31 "name")
+a32=(32 "name")
+a33=(33 "name")
+a34=(34 "name")
+a35=(35 "name")
+a36=(36 "name")
+a37=(37 "name")
+a38=(38 "name")
+a39=(39 "name")
+a40=(40 "name")
+a41=(41 "name")
+a42=(42 "name")
+a43=(43 "name")
+a44=(44 "name")
+a45=(45 "name")
+a46=(46 "name")
+a47=(47 "name")
+a48=(48 "name")
+a49=(49 "name")
+a50=(50 "name")
+a51=(51 "name")
+a52=(52 "name")
+a53=(53 "name")
+a54=(54 "name")
+a55=(55 "name")
+a56=(56 "name")
+a57=(57 "name")
+a58=(58 "name")
+a59=(59 "name")
+a60=(60 "name")
+a61=(61 "name")
+a62=(62 "name")
+a63=(63 "name")
+a64=(64 "name")
+a65=(65 "name")
+a66=(66 "name")
+a67=(67 "name")
+a68=(68 "name")
+a69=(69 "name")
+a70=(70 "name")
+a71=(71 "name")
+a72=(72 "name")
+a73=(73 "name")
+a74=(74 "name")
+a75=(75 "name")
+a76=(76 "name")
+a77=(77 "name")
+a78=(78 "name")
+a79=(79 "name")
+a80=(80 "name")
+a81=(81 "name")
+a82=(82 "name")
+a83=(83 "name")
+a84=(84 "name")
+a85=(85 "name")
+a86=(86 "name")
+a87=(87 "name")
+a88=(88 "name")
+a89=(89 "name")
+a90=(90 "name")
+a91=(91 "name")
+a92=(92 "name")
+a93=(93 "name")
+a94=(94 "name")
+a95=(95 "name")
+a96=(96 "name")
+a97=(97 "name")
+a98=(98 "name")
+a99=(99 "name")
+a100=(100 "name")
+a101=(101 "name")
+a102=(102 "name")
+a103=(103 "name")
+a104=(104 "name")
+a105=(105 "name")
+a106=(106 "name")
+a107=(107 "name")
+a108=(108 "name")
+a109=(109 "name")
+a110=(110 "name")
+a111=(111 "name")
+a112=(112 "name")
+a113=(113 "name")
+a114=(114 "name")
+a115=(115 "name")
+a116=(116 "name")
+a117=(117 "name")
+a118=(118 "name")
+a119=(119 "name")
+a120=(120 "name")
+a121=(121 "name")
+a122=(122 "name")
+a123=(123 "name")
+a124=(124 "name")
+a125=(125 "name")
+a126=(126 "name")
+a127=(127 "name")
+a128=(128 "name")
+a129=(129 "name")
+a130=(130 "name")
+a131=(131 "name")
+a132=(132 "name")
+a133=(133 "name")
+a134=(134 "name")
+a135=(135 "name")
+a136=(136 "name")
+a137=(137 "name")
+a138=(138 "name")
+a139=(139 "name")
+a140=(140 "name")
+a141=(141 "name")
+a142=(142 "name")
+a143=(143 "name")
+a144=(144 "name")
+a145=(145 "name")
+a146=(146 "name")
+a147=(147 "name")
+a148=(148 "name")
+a149=(149 "name")
+a150=(150 "name")
+a151=(151 "name")
+a152=(152 "name")
+a153=(153 "name")
+a154=(154 "name")
+a155=(155 "name")
+a156=(156 "name")
+a157=(157 "name")
+a158=(158 "name")
+a159=(159 "name")
+a160=(160 "name")
+a161=(161 "name")
+a162=(162 "name")
+a163=(163 "name")
+a164=(164 "name")
+a165=(165 "name")
+a166=(166 "name")
+a167=(167 "name")
+a168=(168 "name")
+a169=(169 "name")
+a170=(170 "name")
+a171=(171 "name")
+a172=(172 "name")
+a173=(173 "name")
+a174=(174 "name")
+a175=(175 "name")
+a176=(176 "name")
+a177=(177 "name")
+a178=(178 "name")
+a179=(179 "name")
+a180=(180 "name")
+a181=(181 "name")
+a182=(182 "name")
+a183=(183 "name")
+a184=(184 "name")
+a185=(185 "name")
+a186=(186 "name")
+a187=(187 "name")
+a188=(188 "name")
+a189=(189 "name")
+a190=(190 "name")
+a191=(191 "name")
+a192=(192 "name")
+a193=(193 "name")
+a194=(194 "name")
+a195=(195 "name")
+a196=(196 "name")
+a197=(197 "name")
+a198=(198 "name")
+a199=(199 "name")
+a200=(200 "name")
+a201=(201 "name")
+a202=(202 "name")
+a203=(203 "name")
+a204=(204 "name")
+a205=(205 "name")
+a206=(206 "name")
+a207=(207 "name")
+a208=(208 "name")
+a209=(209 "name")
+a210=(210 "name")
+a211=(211 "name")
+a212=(212 "name")
+a213=(213 "name")
+a214=(214 "name")
+a215=(215 "name")
+a216=(216 "name")
+a217=(217 "name")
+a218=(218 "name")
+a219=(219 "name")
+a220=(220 "name")
+a221=(221 "name")
+a222=(222 "name")
+a223=(223 "name")
+a224=(224 "name")
+a225=(225 "name")
+a226=(226 "name")
+a227=(227 "name")
+a228=(228 "name")
+a229=(229 "name")
+a230=(230 "name")
+a231=(231 "name")
+a232=(232 "name")
+a233=(233 "name")
+a234=(234 "name")
+a235=(235 "name")
+a236=(236 "name")
+a237=(237 "name")
+a238=(238 "name")
+a239=(239 "name")
+a240=(240 "name")
+a241=(241 "name")
+a242=(242 "name")
+a243=(243 "name")
+a244=(244 "name")
+a245=(245 "name")
+a246=(246 "name")
+a247=(247 "name")
+a248=(248 "name")
+a249=(249 "name")
+a250=(250 "name")
+a251=(251 "name")
+a252=(252 "name")
+a253=(253 "name")
+a254=(254 "name")
+a255=(255 "name")
+colorList=("a0" "a1" "a2" "a3" "a4" "a5" "a6" "a7" "a8" "a9" "a10" "a11" "a12" "a13" "a14" "a15" "a16" "a17" "a18" "a19" "a20" "a21" "a22" "a23" "a24" "a25" "a26" "a27" "a28" "a29" "a30" "a31" "a32" "a33" "a34" "a35" "a36" "a37" "a38" "a39" "a40" "a41" "a42" "a43" "a44" "a45" "a46" "a47" "a48" "a49" "a50" "a51" "a52" "a53" "a54" "a55" "a56" "a57" "a58" "a59" "a60" "a61" "a62" "a63" "a64" "a65" "a66" "a67" "a68" "a69" "a70" "a71" "a72" "a73" "a74" "a75" "a76" "a77" "a78" "a79" "a80" "a81" "a82" "a83" "a84" "a85" "a86" "a87" "a88" "a89" "a90" "a91" "a92" "a93" "a94" "a95" "a96" "a97" "a98" "a99" "a100" "a101" "a102" "a103" "a104" "a105" "a106" "a107" "a108" "a109" "a110" "a111" "a112" "a113" "a114" "a115" "a116" "a117" "a118" "a119" "a120" "a121" "a122" "a123" "a124" "a125" "a126" "a127" "a128" "a129" "a130" "a131" "a132" "a133" "a134" "a135" "a136" "a137" "a138" "a139" "a140" "a141" "a142" "a143" "a144" "a145" "a146" "a147" "a148" "a149" "a150" "a151" "a152" "a153" "a154" "a155" "a156" "a157" "a158" "a159" "a160" "a161" "a162" "a163" "a164" "a165" "a166" "a167" "a168" "a169" "a170" "a171" "a172" "a173" "a174" "a175" "a176" "a177" "a178" "a179" "a180" "a181" "a182" "a183" "a184" "a185" "a186" "a187" "a188" "a189" "a190" "a191" "a192" "a193" "a194" "a195" "a196" "a197" "a198" "a199" "a200" "a201" "a202" "a203" "a204" "a205" "a206" "a207" "a208" "a209" "a210" "a211" "a212" "a213" "a214" "a215" "a216" "a217" "a218" "a219" "a220" "a221" "a222" "a223" "a224" "a225" "a226" "a227" "a228" "a229" "a230" "a231" "a232" "a233" "a234" "a235" "a236" "a237" "a238" "a239" "a240" "a241" "a242" "a243" "a244" "a245" "a246" "a247" "a248" "a249" "a250" "a251" "a252" "a253" "a254" "a255")
+
+
+
 formatText()
 {
-    #declarations
-    local formattingCodes=
-    local formattedString="$1"
-    local resetFormatting="$3"
+	#declarations
+	local formattingCodes=
+	local formattedString="$1"
+	local resetFormatting="$3"
 
-    #parse list of formatting codes
-    for code in "$@"; do
-        if [[ "$code" = "$formattedString" ]]; then
-            #this is the string we want to format, so we don't need it
-            continue
-        fi
-            #
-            if [[ -z $formattingCodes ]]; then
-                formattingCodes="${code}"
-            else
-                formattingCodes="${formattingCodes};${code}"
-            fi
-    done
+	#parse list of formatting codes
+	for code in "$@"; do
+		if [[ "$code" = "$formattedString" ]]; then
+			#this is the string we want to format, so we don't need it
+			continue
+		fi
+			#
+			if [[ -z $formattingCodes ]]; then
+				formattingCodes="${code}"
+			else
+				formattingCodes="${formattingCodes};${code}"
+			fi
+	done
 
-    formattedString="\e[${formattingCodes}m${formattedString}"
-    if [[ -z "$resetFormatting" ]]; then
-        formattedString="${formattedString}\e[2${formattingCodes}m"
-    fi
+	formattedString="\e[${formattingCodes}m${formattedString}"
+	if [[ -z "$resetFormatting" ]]; then
+		formattedString="${formattedString}\e[2${formattingCodes}m"
+	fi
 
-    printf "%s" $formattedString
+	printf "%s" $formattedString
 }
 
 colorizeText()
 {
-    local colorCode=$1
-    local colorizedString="\e[38;5;${colorCode}m$2"
-    local resetColorization="$3"
-    local colorResetCode="\e[0m"
+	local colorCode=$1
+	local colorizedString="\e[38;5;${colorCode}m$2"
+	local resetColorization="$3"
+	local colorResetCode="\e[0m"
 
-    #if the user doesn't want a color reset
-    if [[ -z "${resetColorization}" ]]; then
-        colorizedString="${colorizedString}${colorResetCode}"
-    fi
+	#if the user doesn't want a color reset
+	if [[ -z "${resetColorization}" ]]; then
+		colorizedString="${colorizedString}${colorResetCode}"
+	fi
 
-    printf "%s" $colorizedString
+	printf "%s" $colorizedString
 }
 
 colorizeBackground()
 {
-    local colorCode=$1
-    local colorizedString="\e[48;5;${colorCode}m$2"
-    local resetColorization="$3"
-    local colorResetCode="\e[0m"
+	local colorCode=$1
+	local colorizedString="\e[48;5;${colorCode}m$2"
+	local resetColorization="$3"
+	local colorResetCode="\e[0m"
 
-    #if the user doesn't want a color reset
-    if [[ -z "${resetColorization}" ]]; then
-        colorizedString="${colorizedString}${colorResetCode}"
-    fi
+	#if the user doesn't want a color reset
+	if [[ -z "${resetColorization}" ]]; then
+		colorizedString="${colorizedString}${colorResetCode}"
+	fi
 
-    printf "%s" $colorizedString
+	printf "%s" $colorizedString
 }
+
+for colorInformation in ${colorList[@]}; do
+	colorCode=${!colorInformation}
+	eval colorName=\${${colorInformation}[1]}
+	colorSpectrum=$(colorizeBackground $colorCode $colorName)
+	printf "${colorSpectrum}"
+	printf "\n"
+done
