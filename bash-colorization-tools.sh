9650810c8cb03a08f84954a6ae31bb2155759eeb
Dodzi Dzakuma <nexocentric@yahoo.com>
2014-02-13 07:55:47 +0900

    rethinking how to make this useful for other people

--- bash-colorization-tools.sh
+++ bash-colorization-tools.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 #script variables
+COLOR_CODE=0
+COLOR_NAME=1
+COLOR_HEX_CODE=2
+COLOR_HUE=3
 a0=(0 "Black" "#000000" "Black")
 a1=(1 "Maroon" "#800000" "Brown")
 a2=(2 "Green" "#008000" "Green")
@@ -259,6 +263,33 @@ a254=(254 "Gainsboro" "#e4e4e4" "White")
 a255=(255 "White Smoke" "#eeeeee" "White")
 colorList=("a0" "a1" "a2" "a3" "a4" "a5" "a6" "a7" "a8" "a9" "a10" "a11" "a12" "a13" "a14" "a15" "a16" "a17" "a18" "a19" "a20" "a21" "a22" "a23" "a24" "a25" "a26" "a27" "a28" "a29" "a30" "a31" "a32" "a33" "a34" "a35" "a36" "a37" "a38" "a39" "a40" "a41" "a42" "a43" "a44" "a45" "a46" "a47" "a48" "a49" "a50" "a51" "a52" "a53" "a54" "a55" "a56" "a57" "a58" "a59" "a60" "a61" "a62" "a63" "a64" "a65" "a66" "a67" "a68" "a69" "a70" "a71" "a72" "a73" "a74" "a75" "a76" "a77" "a78" "a79" "a80" "a81" "a82" "a83" "a84" "a85" "a86" "a87" "a88" "a89" "a90" "a91" "a92" "a93" "a94" "a95" "a96" "a97" "a98" "a99" "a100" "a101" "a102" "a103" "a104" "a105" "a106" "a107" "a108" "a109" "a110" "a111" "a112" "a113" "a114" "a115" "a116" "a117" "a118" "a119" "a120" "a121" "a122" "a123" "a124" "a125" "a126" "a127" "a128" "a129" "a130" "a131" "a132" "a133" "a134" "a135" "a136" "a137" "a138" "a139" "a140" "a141" "a142" "a143" "a144" "a145" "a146" "a147" "a148" "a149" "a150" "a151" "a152" "a153" "a154" "a155" "a156" "a157" "a158" "a159" "a160" "a161" "a162" "a163" "a164" "a165" "a166" "a167" "a168" "a169" "a170" "a171" "a172" "a173" "a174" "a175" "a176" "a177" "a178" "a179" "a180" "a181" "a182" "a183" "a184" "a185" "a186" "a187" "a188" "a189" "a190" "a191" "a192" "a193" "a194" "a195" "a196" "a197" "a198" "a199" "a200" "a201" "a202" "a203" "a204" "a205" "a206" "a207" "a208" "a209" "a210" "a211" "a212" "a213" "a214" "a215" "a216" "a217" "a218" "a219" "a220" "a221" "a222" "a223" "a224" "a225" "a226" "a227" "a228" "a229" "a230" "a231" "a232" "a233" "a234" "a235" "a236" "a237" "a238" "a239" "a240" "a241" "a242" "a243" "a244" "a245" "a246" "a247" "a248" "a249" "a250" "a251" "a252" "a253" "a254" "a255")
 hueList=("Black" "Brown" "Green" "Blue" "Violet" "Grey" "Red" "Yellow" "White" "Orange")
+
+OPEN_ESCAPE_SEQUENCE="\e["
+CLOSE_ESCAPE_SEQUENCE="m"
+CODE_SEPARATOR=";"
+RESET_ALL_FORMATTING_AND_COLORIZATION="0"
+TEXT_FORMAT_BOLD="1"
+TEXT_FORMAT_DIM="2"
+TEXT_FORMAT_UNDERLINED="4"
+TEXT_FORMAT_BLINK="5"
+TEXT_FORMAT_INVERT="7"
+TEXT_FORMAT_HIDDEN="8"
+
+COLOR_TEXT_CODE=38
+COLOR_BACKGROUND_CODE=48
+
+#if starts with \e add code, but check to see if code already exists
+#replace if exists otherwise add
+#if doesn'T start with \e code then you should wrap in coe
+#you can use regex to achieve this
+
+"\e[1;4;48;5;25;38;5;mHello\e[0m"
+
+checkEscapeSequence()
+{
+	printf "$FUNCNAME"
+}
+
 formatText()
 {
 	#declarations
@@ -318,10 +349,28 @@ colorizeBackground()
 	printf "%s" $colorizedString
 }
 
-for colorInformation in ${colorList[@]}; do
-	eval colorCode=\$${colorInformation}
-	eval colorName=\${${colorInformation}[3]}
-	colorSpectrum=$(colorizeBackground $colorCode "$colorName")
-	printf "${colorSpectrum}"
+for hue in ${hueList[@]}; do
+	printf "${hue}\n"
+	for colorInformation in ${colorList[@]}; do
+		eval colorHue=\${${colorInformation}[$COLOR_HUE]}
+		if [[ "${hue}" == "${colorHue}" ]]; then
+			eval colorCode=\$${colorInformation}
+			eval colorName=\${${colorInformation}[$COLOR_NAME]}
+			colorSpectrum=$(colorizeBackground $colorCode "${colorHue}:${colorName}")
+		 	printf "${colorSpectrum} "
+		fi
+	done
 	printf "\n"
 done
+
+
+
+
+
+# for colorInformation in ${colorList[@]}; do
+# 	eval colorCode=\$${colorInformation}
+# 	eval colorName=\${${colorInformation}[$COLOR_NAME]}
+# 	colorSpectrum=$(colorizeBackground $colorCode "$colorName")
+# 	printf "${colorSpectrum}"
+# 	printf "\n"
+#done
